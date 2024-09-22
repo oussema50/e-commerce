54ee479d1b0899516c27a1c313afce1b37f43c19
oussema <omranioussema3@gmail.com>
2024-09-22 09:22:55 +0100

    create a validator middleware for category route

---
+++ backend/utils/validators/categoryValidator.js
@@ -0,0 +1,23 @@
+const {check,validationResult} = require('express-validator')
+const validatorMiddleware = require('../../middlewares/validatorMiddleware')
+exports.getCategoryValidator = [check('id').isMongoId().withMessage('invalide category id'),validatorMiddleware]
+
+exports.postCategoryValidator = 
+[ 
+    check('name').notEmpty().withMessage('cateogry Name is required ')
+    .isLength({min:3}).withMessage('Too short category name')
+    .isLength({max:3}).withMessage('Too long category name'),
+    validatorMiddleware
+]
+
+exports.updateCategoryValidator = 
+[
+    check('id').isMongoId().withMessage('invalide category id'),
+    validatorMiddleware
+]
+
+exports.deleteCategoryValidator = 
+[
+    check('id').isMongoId().withMessage('invalide category id'),
+    validatorMiddleware
+]
